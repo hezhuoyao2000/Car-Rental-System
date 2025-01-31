a9d2a6419db0abd9070113ec8529c6b26c859d27
hezhuoyao2000 <hezhuoyao2000@gmail.com>
2025-02-01 01:18:44 +1300

    final version

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from database.database_connections import create_connection, initialize_database
 from cli.main_menu import main
 
+import sys
+import os
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
+
 if __name__ == "__main__":
     db_file = "./car_rental.db"
     connection = create_connection(db_file)
